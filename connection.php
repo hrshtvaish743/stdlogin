cd30de80114c30899a52fbd5606142600c8c0d9d
hrshtvaish743 <hrshtvaish743@gmail.com>
2017-05-01 22:43:23 +0530

    mongoclient

--- connection.php
+++ connection.php
@@ -1,5 +1,6 @@
 <?php
-  $uri = "mongodb://test_user:qwerty1234@ds127731.mlab.com:27731/studlogin";
+  $uri = 'mongodb://test_user:hellothere@ds127731.mlab.com:27731/studlogin';
   $conn = new MongoClient($uri);
   $db = $conn->selectDB('studlogin');
+  //mongodb://<dbuser>:<dbpassword>@ds127731.mlab.com:27731/studlogin
  ?>
