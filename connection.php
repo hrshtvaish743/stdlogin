3088e891f5e1822a290524e2abcc8720340dcc26
hrshtvaish743 <hrshtvaish743@gmail.com>
2017-05-01 22:24:15 +0530

    mongoclient

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
   $uri = "mongodb://test_user:qwerty1234@ds127731.mlab.com:27731/studlogin";
-  $conn = new MongoDB\Driver\Manager($uri);
+  $conn = new MongoClient($uri);
   $db = $conn->selectDB('studlogin');
  ?>
